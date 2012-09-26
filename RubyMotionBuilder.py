11bf140587243039ecc09f1aa6af9ca9dc2e00c9
Kentaro Hara <Hara.Kentarow@gmail.com>
2012-09-26 20:38:19 +0900

    Modified code for linter.

--- RubyMotionBuilder.py
+++ RubyMotionBuilder.py
@@ -1,113 +1,121 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
 import os.path
 import re
 
 this_dir = os.path.split(os.path.abspath(__file__))[0]
 
+
 def FindRubyMotionRakefile(dir_name):
-	re_rubymotion = re.compile("Motion")
-	while dir_name != "/":
-		rakefile = os.path.join(dir_name, "Rakefile")
-		if os.path.isfile(rakefile):
-			for line in open(rakefile):
-				if re_rubymotion.search(line):
-					return dir_name
-			return None
-		dir_name = os.path.dirname(dir_name)
-	return None
+    re_rubymotion = re.compile("Motion")
+    while dir_name != "/":
+        rakefile = os.path.join(dir_name, "Rakefile")
+        if os.path.isfile(rakefile):
+            for line in open(rakefile):
+                if re_rubymotion.search(line):
+                    return dir_name
+            return None
+        dir_name = os.path.dirname(dir_name)
+    return None
+
 
 class RubyMotionBuild(sublime_plugin.WindowCommand):
-	def run(self, build_target=None):
-		view = self.window.active_view()
-		if not view:
-			return
-		dir_name = FindRubyMotionRakefile(os.path.split(view.file_name())[0])
-		if dir_name:
-			sh_name = os.path.join(this_dir, "rubymotion_build.sh")
-			cmd = "rake build"
-			if build_target and build_target != "all":
-				cmd += ":" + build_target
+    def run(self, build_target=None):
+        view = self.window.active_view()
+        if not view:
+            return
+        dir_name = FindRubyMotionRakefile(os.path.split(view.file_name())[0])
+        if dir_name:
+            sh_name = os.path.join(this_dir, "rubymotion_build.sh")
+            cmd = "rake build"
+            if build_target and build_target != "all":
+                cmd += ":" + build_target
 
-			settings = sublime.load_settings("RubyMotion.sublime-settings")
-			env_file = settings.get("rubymotion_build_env_file", "")
+            settings = sublime.load_settings("RubyMotion.sublime-settings")
+            env_file = settings.get("rubymotion_build_env_file", "")
 
-			file_regex = "^(...*?):([0-9]*):([0-9]*)"
-			self.window.run_command("exec", {"cmd": ["sh", sh_name, cmd, env_file], "working_dir": dir_name, "file_regex": file_regex})
+            file_regex = "^(...*?):([0-9]*):([0-9]*)"
+            self.window.run_command("exec", {"cmd": ["sh", sh_name, cmd, env_file], "working_dir": dir_name, "file_regex": file_regex})
+
 
 class RubyMotionClean(sublime_plugin.WindowCommand):
-	def run(self):
-		view = self.window.active_view()
-		if not view:
-			return
-		dir_name = FindRubyMotionRakefile(os.path.split(view.file_name())[0])
-		if dir_name:
-			sh_name = os.path.join(this_dir, "rubymotion_build.sh")
-			cmd = "rake clean"
+    def run(self):
+        view = self.window.active_view()
+        if not view:
+            return
+        dir_name = FindRubyMotionRakefile(os.path.split(view.file_name())[0])
+        if dir_name:
+            sh_name = os.path.join(this_dir, "rubymotion_build.sh")
+            cmd = "rake clean"
 
-			settings = sublime.load_settings("RubyMotion.sublime-settings")
-			env_file = settings.get("rubymotion_build_env_file", "")
+            settings = sublime.load_settings("RubyMotion.sublime-settings")
+            env_file = settings.get("rubymotion_build_env_file", "")
 
-			file_regex = "^(...*?):([0-9]*):([0-9]*)"
-			self.window.run_command("exec", {"cmd": ["sh", sh_name, cmd, env_file], "working_dir": dir_name, "file_regex": file_regex})
+            file_regex = "^(...*?):([0-9]*):([0-9]*)"
+            self.window.run_command("exec", {"cmd": ["sh", sh_name, cmd, env_file], "working_dir": dir_name, "file_regex": file_regex})
+
 
 class RubyMotionRun(sublime_plugin.WindowCommand):
-	def run(self, options=""):
-		view = self.window.active_view()
-		if not view:
-			return
-		dir_name = FindRubyMotionRakefile(os.path.split(view.file_name())[0])
-		if dir_name:
-			sh_name = os.path.join(this_dir, "rubymotion_run.sh")
-			file_regex = "^(...*?):([0-9]*):([0-9]*)"
-			# build console is not required for Run
-			self.window.run_command("hide_panel", {"panel": "output.exec"})
-			settings = sublime.load_settings("Preferences.sublime-settings")
-			show_panel_on_build = settings.get("show_panel_on_build", True)
-			if show_panel_on_build:
-				# temporary setting to keep console visibility
-				settings.set("show_panel_on_build", False)
-			self.window.run_command("exec", {"cmd": ["sh", sh_name, dir_name, options], "working_dir": dir_name, "file_regex": file_regex})
-			# setting recovery
-			settings.set("show_panel_on_build", show_panel_on_build)
+    def run(self, options=""):
+        view = self.window.active_view()
+        if not view:
+            return
+        dir_name = FindRubyMotionRakefile(os.path.split(view.file_name())[0])
+        if dir_name:
+            sh_name = os.path.join(this_dir, "rubymotion_run.sh")
+            file_regex = "^(...*?):([0-9]*):([0-9]*)"
+            # build console is not required for Run
+            self.window.run_command("hide_panel", {"panel": "output.exec"})
+            settings = sublime.load_settings("Preferences.sublime-settings")
+            show_panel_on_build = settings.get("show_panel_on_build", True)
+            if show_panel_on_build:
+                # temporary setting to keep console visibility
+                settings.set("show_panel_on_build", False)
+            self.window.run_command("exec", {"cmd": ["sh", sh_name, dir_name, options], "working_dir": dir_name, "file_regex": file_regex})
+            # setting recovery
+            settings.set("show_panel_on_build", show_panel_on_build)
+
 
 class RubyMotionDeploy(sublime_plugin.WindowCommand):
-	def run(self):
-		view = self.window.active_view()
-		if not view:
-			return
-		dir_name = FindRubyMotionRakefile(os.path.split(view.file_name())[0])
-		if dir_name:
-			sh_name = os.path.join(this_dir, "rubymotion_build.sh")
-			cmd = "rake device"
+    def run(self):
+        view = self.window.active_view()
+        if not view:
+            return
+        dir_name = FindRubyMotionRakefile(os.path.split(view.file_name())[0])
+        if dir_name:
+            sh_name = os.path.join(this_dir, "rubymotion_build.sh")
+            cmd = "rake device"
 
-			settings = sublime.load_settings("RubyMotion.sublime-settings")
-			env_file = settings.get("rubymotion_build_env_file", "")
+            settings = sublime.load_settings("RubyMotion.sublime-settings")
+            env_file = settings.get("rubymotion_build_env_file", "")
 
-			file_regex = "^(...*?):([0-9]*):([0-9]*)"
-			self.window.run_command("exec", {"cmd": ["sh", sh_name, cmd, env_file], "working_dir": dir_name, "file_regex": file_regex})
+            file_regex = "^(...*?):([0-9]*):([0-9]*)"
+            self.window.run_command("exec", {"cmd": ["sh", sh_name, cmd, env_file], "working_dir": dir_name, "file_regex": file_regex})
+
 
 class GenerateRubyMotionSyntax(sublime_plugin.WindowCommand):
-	def run(self):
-		rb_name = os.path.join(this_dir, "rubymotion_syntax_generator.rb")
-		self.window.run_command("exec", {"cmd": ["ruby", rb_name], "working_dir": this_dir})
+    def run(self):
+        rb_name = os.path.join(this_dir, "rubymotion_syntax_generator.rb")
+        self.window.run_command("exec", {"cmd": ["ruby", rb_name], "working_dir": this_dir})
+
 
 class GenerateRubyMotionCompletions(sublime_plugin.WindowCommand):
-	def run(self):
-		rb_name = os.path.join(this_dir, "rubymotion_completion_generator.rb")
-		bridge_support_dir = "/Library/RubyMotion/data/5.1/BridgeSupport/"
-		self.window.run_command("exec", {"cmd": ["ruby", rb_name, bridge_support_dir], "working_dir": this_dir})
+    def run(self):
+        rb_name = os.path.join(this_dir, "rubymotion_completion_generator.rb")
+        bridge_support_dir = "/Library/RubyMotion/data/5.1/BridgeSupport/"
+        self.window.run_command("exec", {"cmd": ["ruby", rb_name, bridge_support_dir], "working_dir": this_dir})
+
 
 class SetRubyMotionSyntax(sublime_plugin.EventListener):
-	def set_rubymotion_syntax(self, view):
-		dir_name, file_name = os.path.split(view.file_name())
-		ext = os.path.splitext(file_name)[1]
-		if ext == ".rb" or file_name == "Rakefile":
-			if FindRubyMotionRakefile(dir_name):
-				view.set_syntax_file(os.path.join(this_dir, "RubyMotion.tmLanguage"))
+    def set_rubymotion_syntax(self, view):
+        dir_name, file_name = os.path.split(view.file_name())
+        ext = os.path.splitext(file_name)[1]
+        if ext == ".rb" or file_name == "Rakefile":
+            if FindRubyMotionRakefile(dir_name):
+                view.set_syntax_file(os.path.join(this_dir, "RubyMotion.tmLanguage"))
 
     def on_load(self, view):
         self.set_rubymotion_syntax(view)
 
     def on_pre_save(self, view):
         self.set_rubymotion_syntax(view)
-
