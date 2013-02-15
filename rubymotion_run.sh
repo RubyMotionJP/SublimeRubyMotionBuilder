f2a03b91839127584365dfabafbf33f71bc346a9
Ian Dundas <contact@iandundas.co.uk>
2013-02-15 12:52:50 +0700

    improved previous commit to avoid race conditions, code cleanup

--- rubymotion_run.sh
+++ rubymotion_run.sh
@@ -13,22 +13,13 @@ try
     tell application "Terminal"
         activate
         try
-						close (every window whose custom title is "${TERMINAL_ID}")
 						close (every window whose custom title is "rake")
-	          do script "cd \"${PROJECT_DIR}\""
-
+						delay 0.5
+		        do script "cd \"${PROJECT_DIR}\""
+				    do script "rake ${OPTIONS}" in front window
 				on error errs number errn
 						display dialog errs & return & "Error: " & (errn as string)
-            do script "alias quit='' && cd \"${PROJECT_DIR}\" && clear"
         end try
-    		
-				delay 0.1
-
-		    tell window 1
-		        set custom title to "${TERMINAL_ID}"
-		    end tell
-
-		    do script "rake ${OPTIONS}" in front window
 	    end tell
 end try
 END
